8995d49afacc0ad70792cfa6cc60277bd9dfb8cb
spookydonut <github@spooksoftware.com>
2019-06-02 19:12:35 +0800

    an example program for the robot that i was given.

---
+++ irbumper.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from ev3dev2.port import LegoPort
+from ev3dev2.sensor import Sensor
+from ev3dev2.sensor.lego import InfraredSensor
+from MuxedSensors import TouchMuxed
+from ev3dev2.motor import Motor, MoveTank, SpeedPercent, OUTPUT_B, OUTPUT_D
+
+middle = TouchMuxed(address = "in1:i2c80:mux1")
+right = TouchMuxed(address = "in1:i2c81:mux2")
+left = TouchMuxed(address = "in1:i2c82:mux3")
+
+ir = InfraredSensor()
+
+tank = MoveTank(OUTPUT_B, OUTPUT_D)
+
+current_speed = 100
+
+def go_forward():
+	tank.on(-SpeedPercent(current_speed), -SpeedPercent(current_speed))
+
+def back_up():
+	tank.on_for_seconds(-SpeedPercent(50), -SpeedPercent(50), 2)
+
+def rotate_left():
+	tank.on_for_rotations(SpeedPercent(50), -SpeedPercent(50), 0.5)
+
+def rotate_right():
+	tank.on_for_rotations(-SpeedPercent(50), SpeedPercent(50), 0.5)
+
+
+
+while True:
+	if ir.proximity < 90:
+		current_speed = max(30, ir.proximity)
+	else:
+		current_speed = 100
+
+	if middle.is_pressed:
+		back_up()
+		rotate_left()
+		rotate_left()
+		continue
+	if left.is_pressed:
+		back_up()
+		rotate_right()
+		continue
+	if right.is_pressed:
+		back_up()
+		rotate_left()
+		continue
+	
+	go_forward()
+
